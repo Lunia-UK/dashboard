5d824ae50b8ce4a2b7a280f48c35e62ecac3c141
Lunia-UK <mat.wattrelos@yahoo.com>
2020-05-15 23:34:54 +0200

    add menu, grid to index

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Dashboard</title>
+</head>
+<body>
+    <header></header>
+    <aside>
+        <nav>
+            <a href=""><img src="assets/svg/home.svg" alt="Home"/></a>
+            <a href=""><img src="assets/svg/calendar.svg" alt="Calendar"/></a>
+            <a href=""><img src="assets/svg/bike.svg" alt="Sport"/></a>
+            <a href=""><img src="assets/svg/tasks.svg" alt="Tasks"/></a>
+            <a href=""><img src="assets/svg/project.svg" alt="Projects"/></a>
+            <a href=""><img src="assets/svg/challenge.svg" alt="Challenge"/></a>
+            <a href=""><img src="assets/svg/note.svg" alt="Notes"/></a>
+            <a href=""><img src="assets/svg/sleep.svg" alt="Sleep"/></a>
+        </nav>
+    </aside>
+    <main>
+        <div class="distance"></div>
+        <div class="calendar"></div>
+        <div class="daily"></div>
+        <div class="objectif"></div>
+        <div class="sleep"></div>
+        <div class="sport"></div>
+        <div class="daily_sport"></div>
+        <div class="alert"></div>
+        <div class="calc"></div>
+        <div class="note"></div>
+    </main>
+</body>
+</html>
