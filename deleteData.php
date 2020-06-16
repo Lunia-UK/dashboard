c1feb86e31e5713a078036be1cddd2de382195a1
Lunia-UK <mat.wattrelos@yahoo.com>
2020-06-16 18:05:33 +0100

    Change saveData function. Create form to save data. Add deletedData function with ajax

--- deleteData.php
+++ deleteData.php
@@ -1,4 +1,5 @@
 <?php 
     require_once 'include/db.php';
-    $sql = "DELETE FROM calendar WHERE date = $_GET["date"]";
+    $sql = "DELETE FROM calendar WHERE id = '".$_GET["id"]."'";
+    $result = $mysqli->query($sql);
 ?>
--- loadData.php
+++ loadData.php
@@ -1,6 +1,6 @@
 <?php 
     require_once 'include/db.php';
-    $sql = "SELECT id, title, date FROM calendar WHERE date > '".$_GET["dateBegin"]."' AND date < '".$_GET["dateEnd"]."'";
+    $sql = "SELECT id, title, date, description FROM calendar WHERE date > '".$_GET["dateBegin"]."' AND date < '".$_GET["dateEnd"]."'";
     $result = $mysqli->query($sql);
     $arrData = $result->fetch_all();
     $arrData = json_encode($arrData);
